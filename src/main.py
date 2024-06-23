8b29bf5b95c6338291648b61e2590ad337969937
Botond Vastag <vastag.botond@gmail.com>
2024-06-23 19:05:19 +0200

    add sensor to sense the environment

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 from protocol import Protocol
-
+from sensor import *
 
 def main():
     p = Protocol("localhost", 10000)
     p.Connect()
-    print(p.GetData())
-    print(p.GetData())
-    p.SendData("1 1\n")
-    print(p.GetData())
-        
+    sensor = Sensor(p.GetData())
+    sensor.Sense(p.GetData())
+    print(vars(sensor.environment))
+    print(len(sensor.vision.grid))
+    print(vars(sensor.physics))
 
 if __name__ == "__main__":
     main()
---
+++ src/sensor.py
@@ -0,0 +1,41 @@
+class Environment:
+    def __init__(self, input: list[str]):
+        self.height = int(input[0])
+        self.width = int(input[1])
+        self.num_of_players = int(input[2])
+        self.vis_radius = int(input[3])
+
+class Physics:
+    def __init__(self, input: list[str]):
+        self.x = int(input[0])
+        self.y = int(input[1])
+        self.vx = int(input[2])
+        self.vy = int(input[3])
+
+class Vision:
+    def __init__(self, players: list[str], grid: list[str]):
+        self.players = []
+        self.grid = []
+        for line in players:
+            components = line.split()
+            number_list = [int(x) for x in components]
+            self.players.append(number_list)
+        for line in grid:
+            components = line.split()
+            number_list = [int(x) for x in components]
+            self.grid.append(number_list)
+        
+class Sensor:
+    def __init__(self, input: str):
+        temp = input.split()
+        self.environment = Environment(temp)
+        
+    def Sense(self, input: str):
+        lines = input.splitlines()
+        self.physics = Physics(lines[0].split())
+        lines = lines[1:]
+        players = lines[:self.environment.num_of_players]
+        lines = lines[self.environment.num_of_players:]
+        grid = lines[:2 * self.environment.vis_radius + 1]
+        self.vision = Vision(players, grid)
+        
